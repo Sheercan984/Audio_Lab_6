619f768ca9ef1fac7c30d3c4ff002efe57bf843a
Patryk Szczesiak <p.szczesiak984@gmail.com>
2021-02-04 09:44:41 +0100

    Lab6_FMOD

---
+++ Assets/_Code/Ambient.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ambient : MonoBehaviour
+{
+    [SerializeField] private GameObject outsideAudio;
+    [SerializeField] private GameObject insideAudo;
+    private bool isPlayerInside = false;
+
+    private void Start()
+    {
+        outsideAudio.SetActive(true);
+        insideAudo.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPlayerInside = !isPlayerInside;
+            if (isPlayerInside)
+            {
+                insideAudo.SetActive(true);
+                outsideAudio.SetActive(false);
+            }
+            else
+            {
+                insideAudo.SetActive(false);
+                outsideAudio.SetActive(true);
+            }
+        }
+    }
+}
